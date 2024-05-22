13be7288876c246eb495630f4acc11572cbcae63
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-05-22 15:46:15 +1200

    Styled Components - Global Styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
 
 const StyledApp = styled.div`
 background-color: orangered;
@@ -29,12 +16,16 @@ padding: 20px;
 `
 function App() {
   return (
+    <>
+    <GlobalStyles />
     <StyledApp>
       <H1>Hello Wild Oasis</H1>
       <Button onClick={() => alert("You clicked me")}>Click Me</Button>
       <Button onClick={() => alert("You clicked me")}>Check Out</Button>
       <Input type='number' placeholder='numbers only' />
+      <Input type='number' placeholder='numbers only' />
     </StyledApp>
+    </>
   );
 }
 
