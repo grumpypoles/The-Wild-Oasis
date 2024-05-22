6dcd0509a5bc85d31eb868e38468ee29a2c15b90
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-05-22 15:01:07 +1200

    Styled Components - Introduction.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+background-color: orangered;
+padding: 20px;
+`
 function App() {
   return (
-    <div>
-      Hello Wild Oasis
-    </div>
-  )
+    <StyledApp>
+      <H1>Hello Wild Oasis</H1>
+      <Button onClick={() => alert("You clicked me")}>Click Me</Button>
+      <Button onClick={() => alert("You clicked me")}>Check Out</Button>
+      <Input type='number' placeholder='numbers only' />
+    </StyledApp>
+  );
 }
 
-export default App
+export default App;
