7d1949328cf6b2719a7e6cc0ecc5c2a138d664b9
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-05-23 09:41:18 +1200

    Setting up App Layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import GlobalStyles from '../src/styles/GlobalStyles'
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Dashboard from "./pages/Dashboard"
-import Bookings from "./pages/Bookings"
-import Users from "./pages/Users"
-import Cabins from "./pages/Cabins"
-import Settings from "./pages/Settings"
-import Account from "./pages/Account"
-import Login from "./pages/Login"
-import PageNotFound from "./pages/PageNotFound"
+import GlobalStyles from "../src/styles/GlobalStyles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Users from "./pages/Users";
+import Cabins from "./pages/Cabins";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
-   <GlobalStyles />
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate replace to ="dashboard" />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="users" element={<Users />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="account" element={<Account />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="users" element={<Users />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
