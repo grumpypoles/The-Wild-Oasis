6b7bfcb4d4681d8c296653de74a79492e67cfae5
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-05-22 17:06:22 +1200

    Styled Components - Building More Reusable Styled Components.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,41 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
-
-
-
-
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-background-color: orangered;
-padding: 20px;
-`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
 function App() {
   return (
     <>
-    <GlobalStyles />
-    <StyledApp>
-      <Heading as='h1'>Hello Wild Oasis</Heading>
-      <Heading as='h3'>Check in and Out</Heading>
-      <Button onClick={() => alert("You clicked me")}>Click Me</Button>
-      <Button onClick={() => alert("You clicked me")}>Check Out</Button>
-      <Heading as='h2'>Form</Heading>
-      <Input type='number' placeholder='numbers only' />
-      <Input type='number' placeholder='numbers only' />
-    </StyledApp>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello Wild Oasis</Heading>
+            <div>
+              <Heading as="h3">Check in and Out</Heading>
+              <Button onClick={() => alert("You clicked in")}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("You clicked out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h2">Form</Heading>
+            <form>
+              <Input type="number" placeholder="numbers only" />
+              <Input type="number" placeholder="numbers only" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }
