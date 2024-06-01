9f791bd6894105457b3a4f7f3aa84fcf3bd75b46
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-06-01 12:54:23 +1200

    Reusable Context Menu - The setup.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 
 
@@ -29,6 +30,7 @@ const {isLoading, cabins} = useCabins()
   if (isLoading) return <Spinner />;
 
   return (
+    <Menus>
     <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
       <Table.Header>
         <div></div>
@@ -43,6 +45,7 @@ const {isLoading, cabins} = useCabins()
       )} />
       
     </Table>
+    </Menus>
   );
 }
 
