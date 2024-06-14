1f2b8e6f6e0e25c0b77877ad85560d1c5032e40c
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-06-14 13:21:46 +1200

    Authentication - User Login with Supabase.

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -12,8 +12,12 @@ ${props => props.as === 'h1' && css`font-size: 3rem;
 ${props => props.as === 'h2' && css`font-size: 2rem;
   font-weight: 600;`}
   
-${props => props.as === 'h3' && css`font-size: 1rem;
+${props => props.as === 'h3' && css`font-size: 2rem;
   font-weight: 500;`}
+
+${props => props.as === 'h4' && css`font-size: 3rem;
+  font-weight: 600;
+    text-align: center;`}
   
 line-height: 1.4;
  
