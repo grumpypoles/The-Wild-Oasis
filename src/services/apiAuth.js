cba7b4ff5827fafb21816f5c7650f1021eda1e44
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-06-19 16:31:49 +1200

    Authorisation - Updating user full name, avatar and password.

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,3 +1,5 @@
+import { supabaseUrl } from "./supabase";
+
 import supabase from "./supabase.js";
 
 export async function signup({ fullName, email, password }) {
@@ -11,6 +13,7 @@ export async function signup({ fullName, email, password }) {
       },
     },
   });
+
   if (error) throw new Error(error.message);
 
   return data;
@@ -43,7 +46,33 @@ export const getCurrentUser = async () => {
 
 export async function logout() {
   const { error } = await supabase.auth.signOut();
-  if (error) {
-    throw new Error(error.message);
-  }
+  if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  //1. Update password or fullName
+
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if(!avatar) return data
+
+  //2. Upload the avatra image
+
+  const fileName = `avatar-${data.user.id}-${Math.random()}`
+  const {error: storageError }=  await supabase.storage.from('avatars').upload(fileName,avatar)
+  if (storageError) throw new Error(storageError.message);
+
+  //3. Upadate avatar in the user
+
+  const {data: updatedUser, error: error2 } = supabase.auth.updateUser({
+    data:{
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`
+    }
+  })
+  if (error2) throw new Error(error2.message);
+  return updatedUser
 }
