9dfad2816635ea106da386ad7489087e2d09e676
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-06-23 16:01:14 +1200

    Dashboard - Pie Chart.

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "../../ui/Heading";
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -51,7 +54,7 @@ const startDataLight = [
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 2,
     color: "#3b82f6",
   },
   {
@@ -130,3 +133,39 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+function DurationChart({ confirmedStays }) {
+
+const {isDarkMode} = useDarkMode()
+const startData = isDarkMode ? startDataDark : startDataLight
+const data = prepareData(startData, confirmedStays)
+  return (
+    <ChartBox>
+      <Heading as="h2">Stay Duration summary</Heading>
+      <ResponsiveContainer width='100%' height={240}>
+        <PieChart>
+          <Pie
+            data={data}
+            nameKey="duration"
+            dataKey="value"
+            innerRadius={85}
+            outerRadius={110}
+            cx="40%"
+            cy="50%"
+            paddingAngle={3}
+          >
+            {startDataLight.map(entry => <Cell fill={entry.color} stroke={entry.color} key={entry.duration}/>)}
+          </Pie>
+          <Tooltip/>
+          <Legend verticalAlign="middle" align="right" width='30%'
+            layout="vertical"
+            iconSize={15}
+            iconType="circle"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
