7f77607c5a8eff36ccdf08b653e2b39d05a2c8a5
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-06-25 14:29:20 +1200

    Production First Version.

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("fred.fredovich@fred.com");
-  const [password, setPassword] = useState("fred@1234");
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
